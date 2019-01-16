a27cf9b79f69507c5a93d1159f0c15997d813d1b
MShestukhin <shist95@mail.ru>
2019-01-16 16:47:52 +0800

    add route

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Router from 'vue-router'
+import Main from './components/Main.vue'
+import  HelloWorld from './components/HelloWorld'
 // register globally
 import YmapPlugin from 'vue-yandex-maps'
 Vue.use(YmapPlugin)
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name:'home',
+            component: Main,
+        },
+        {
+            path: '/post',
+            name:'post',
+            component: HelloWorld,
+            props: true,
+        },
+    ]
+})
 
 // or for a single instance
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
